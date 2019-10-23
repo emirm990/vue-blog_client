f06202a5f34394ee542ba6c43ee920ecdd0607c2
Emir Mekić <emir.mekic@live.com>
2019-10-23 18:30:53 +0200

    routes

--- src/BlogService.js
+++ src/BlogService.js
@@ -1,31 +1,48 @@
-import axios from "axios";
+import axios from 'axios';
 
-const url = "http://localhost:5000/api/blogs";
+const url = 'http://localhost:5000/api/blogs';
 
 class BlogService {
-  // Get blogs
-  static getBlogs() {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const res = await axios.get(url);
-        const data = res.data;
+	// Get blogs
+	static getBlogs() {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const res = await axios.get(url);
+				const data = res.data;
 
-        resolve(
-          data.map(blog => ({
-            ...blog,
-            createdAt: new Date(blog.createdAt)
-          }))
-        );
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-  // Create Post
-  static insertBlog(text) {
-    return axios.post(url, {
-      text
-    });
-  }
+				resolve(
+					data.map((blog) => ({
+						...blog,
+						createdAt: new Date(blog.createdAt)
+					}))
+				);
+			} catch (err) {
+				reject(err);
+			}
+		});
+	}
+	// Create Post
+	static insertBlog(title, text) {
+		return axios.post(url, {
+			title,
+			text
+		});
+	}
+	// Delete Post
+	static deleteBlog(id) {
+		return axios.delete(url, {
+			data: {
+				id
+			}
+		});
+	}
+
+	static editBlog(id) {
+		return axios.get(url, {
+			params: {
+				id
+			}
+		});
+	}
 }
 export default BlogService;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="about">
-    <wysiwyg v-model="myHTML" />
-    <button @click="createBlog">Submit</button>
+  <div class="containter">
+    <form class="about">
+      <input type="text" placeholder="Blog title" required v-model="title" />
+      <wysiwyg v-model="myHTML" />
+      <input type="submit" @click.prevent="createBlog" value="Submit" />
+    </form>
+    <div class="blog" v-for="blog in blogs" v-bind:key="blog._id">
+      <div class="blog-header">
+        <h2 class="blog-title">{{blog.title}}</h2>
+        <p
+          class="blog-date"
+        >{{`${blog.createdAt.getDate()}/${blog.createdAt.getMonth()}/${blog.createdAt.getFullYear()}`}}</p>
+        <button @click="deleteBlog(blog._id)">Delete blog</button>
+        <button @click="editBlog(blog._id)">Edit blog {{blog._id}}</button>
+      </div>
+    </div>
   </div>
 </template>
-
+  
 <script>
 import BlogService from "../BlogService";
 
@@ -12,17 +25,41 @@ export default {
   name: "Editor",
   data() {
     return {
+      blogs: [],
       myHTML: "",
-      error: ""
+      title: "",
+      error: "",
+      blog: ""
     };
   },
   methods: {
     async createBlog() {
       try {
-        await BlogService.insertBlog(this.myHTML);
+        await BlogService.insertBlog(this.title, this.myHTML);
+        this.blogs = await BlogService.getBlogs();
+        this.myHTML = "";
+        this.title = "";
       } catch (err) {
         this.error = err.message;
       }
+    },
+    async deleteBlog(id) {
+      await BlogService.deleteBlog(id);
+      this.blogs = await BlogService.getBlogs();
+    },
+    async editBlog(id) {
+      try {
+        this.blog = await BlogService.editBlog(id);
+      } catch (err) {
+        this.error = err.message;
+      }
+    }
+  },
+  async created() {
+    try {
+      this.blogs = await BlogService.getBlogs();
+    } catch (err) {
+      this.error = err.message;
     }
   }
 };
